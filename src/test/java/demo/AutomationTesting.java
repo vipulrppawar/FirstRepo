95cfb1a7449686ce0dc1118fa63f2ce857bc4732
pvpatil5 <pavan@freshprints.com>
2023-09-25 09:02:56 +0530

    First commit

---
+++ src/test/java/demo/AutomationTesting.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class AutomationTesting 
+{
+
+	public static void main(String[] args)
+	{
+	
+		System.out.println(5+15);
+	}
+	
+}
---
+++ src/test/java/demo/HelloWorld.java
@@ -0,0 +1,19 @@
+package demo;
+
+//package name should be always in small letters
+
+//Class name should be always in camel case
+public class HelloWorld 
+{
+
+	// Main method = With out this method program will not run
+	// Shortcut way to create = just type "main" and hit control+space
+	// control+space = suggestion
+	// save program = control+s
+	public static void main (String[] args) 
+	{
+		// This line shows output to the user
+		System.out.println("Hello Java");
+		System.out.println("Hello Java....to All");
+	}
+}
